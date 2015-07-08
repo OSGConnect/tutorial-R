6f0acd8c1324d3ff782b27f55935408e215d9b6a
echism <echism@iu.edu>
2015-07-08 14:42:26 -0400

    made tutorial wrapper conform to documentation
    in the documentation, the wrapper checks for arguments. Updated this wrapper to reflect that.

--- R-wrapper.sh
+++ R-wrapper.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+EXPECTED_ARGS=1
+
+if [ $# -ne $EXPECTED_ARGS ]; then
+  echo "Usage: R-wrapper.sh file.R"
+  exit 1
+else
   source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/current/init/bash
   module load R
   Rscript $1
+fi
